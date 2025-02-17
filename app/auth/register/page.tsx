6b734f5e734401c9edf49d1f23c56dca26dcd2de
Zubair Abubakar <zuby52@gmail.com>
2025-02-17 22:27:13 +0100

    [UM-2] add user registration form

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { RegisterForm } from '@/components/auth/register-form';
+
+const RegisterPage = () => {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default RegisterPage;
