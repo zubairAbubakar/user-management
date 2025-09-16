6454b0f5c706bc0e31f4fb58e372fed2429d7e9b
Zubair Abubakar <zuby52@gmail.com>
2025-09-16 08:01:55 +0100

    [UM-6] update env variables

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -7,9 +7,14 @@ import { FcGoogle } from 'react-icons/fc';
 import { Button } from '@/components/ui/button';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
+import { useSearchParams } from 'next/navigation';
+
 export const Socials = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl');
+
   const onClick = (provider: 'google' | 'github') => {
-    signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+    signIn(provider, { callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT });
   };
 
   return (
